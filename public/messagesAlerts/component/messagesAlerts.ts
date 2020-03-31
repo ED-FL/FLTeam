c11c5cf256088dc6a65bd28f2c954d83daf9ef0e
yael <yael19961@gmail.com>
2020-03-31 19:41:08 +0300

    start component

--- public/messagesAlerts/component/messagesAlerts.ts
+++ public/messagesAlerts/component/messagesAlerts.ts
@@ -2,8 +2,14 @@ angular.module("app").component("messagesAlerts", {
   templateUrl: "./messagesAlerts.html",
   bindings: {},
   controller: function() {
+    this.currentAlerts = ["yael", "dolev", "ela"];
+    this.$onInit = function() {};
     this.showLastMessage = function() {};
     this.showAllMessages = function() {};
     this.dontShowMessages = function() {};
+
+    this.clearScrean = function() {};
+    this.activeateButton = function() {};
+    this.addAlert = function() {};
   }
 });
