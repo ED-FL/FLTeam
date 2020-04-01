0b1f90bd9a314ffe0373ec771c9e74df3d1ab055
yael <yael19961@gmail.com>
2020-04-01 09:44:55 +0300

    alart button

--- public/messagesAlerts/component/messagesAlerts.ts
+++ public/messagesAlerts/component/messagesAlerts.ts
@@ -1,15 +1,41 @@
+enum ALARMS_ICONS {
+  LAST_ALARM = "alarm",
+  NO_ALARMS = "alarm_off",
+  ALL_ALARMS = "alarm_add"
+}
+
 angular.module("app").component("messagesAlerts", {
   templateUrl: "./messagesAlerts.html",
   bindings: {},
   controller: function() {
-    this.currentAlerts = ["yael", "dolev", "ela"];
+    this.alarmsIcons = [
+      ALARMS_ICONS.LAST_ALARM,
+      ALARMS_ICONS.ALL_ALARMS,
+      ALARMS_ICONS.NO_ALARMS
+    ];
+    this.currentIcon = ALARMS_ICONS.LAST_ALARM;
+    this.currentAlerts;
+    this.originatorEv;
+
     this.$onInit = function() {};
-    this.showLastMessage = function() {};
-    this.showAllMessages = function() {};
-    this.dontShowMessages = function() {};
+
+    this.openMenu = function($mdMenu, ev) {
+      this.originatorEv = ev;
+      $mdMenu.open(ev);
+    };
+    this.activeateButton = function() {};
+
+    this.showLastMessage = function() {
+      this.currentIcon = ALARMS_ICONS.LAST_ALARM;
+    };
+    this.showAllMessages = function() {
+      this.currentIcon = ALARMS_ICONS.ALL_ALARMS;
+    };
+    this.dontShowMessages = function() {
+      this.currentIcon = ALARMS_ICONS.NO_ALARMS;
+    };
 
     this.clearScrean = function() {};
-    this.activeateButton = function() {};
     this.addAlert = function() {};
   }
 });
